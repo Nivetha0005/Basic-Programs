c8a6fd023f70acd1e92bdb358b93c284714032ec
Nivetha0005 <nivetha.durairaj05@gmail.com>
2025-07-04 21:50:54 +0530

    CommentLineArg.java
    a Program that accepts two Strings as command line arguments and generate 
    the output in the required format. 
    Example :
    If the two command line arguments are Wipro and Bangalore then the output generated should 
    be Wipro Technologies Bangalore.

--- CommentLineArg.java
+++ CommentLineArg.java
@@ -1,15 +1,15 @@
-import java.util.*;
 public class CommentLineArg {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		String[] a=new String[2];
-		System.out.println("Enter the company name:");
-		a[0]=s.next();
-		System.out.println("Enter the location:");
-		a[1]=s.next();
-		System.out.println(a[0] + " Technologies " + a[1]);
-		s.close();
+	if (args.length != 2) {
+            System.out.println("Please enter exactly two arguments: <CompanyName> <City>");
+            return;
+        }
+
+        String companyName = args[0];
+        String city = args[1];
+
+        System.out.println(companyName + " Technologies " + city);
 	}
 
 }
