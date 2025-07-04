3a91a5d438fc04c2b39e67dcc6777d683c2e0bdc
Nivetha0005 <nivetha.durairaj05@gmail.com>
2025-07-04 20:58:57 +0530

    Add files via upload

---
+++ Positive-negative.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter your number:");
+		int n=s.nextInt();
+		if(n<0) {
+			System.out.println("the number "+n+" is negative.");
+		}
+		else if(n>0) {
+			System.out.println("the number "+n+" is positive.");
+		}
+		else {
+			System.out.println("the number "+n+" is zero.");
+		}
+		s.close();
+	}
+
+}
