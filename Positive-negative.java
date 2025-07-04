a17ea3e94a8da50a6e3c9b18c019b523a32a28f9
Nivetha0005 <nivetha.durairaj05@gmail.com>
2025-07-04 21:25:28 +0530

    Positive-negative.java
    a program to check if a given integer number is Positive, Negative, or Zero.

--- Positive-negative.java
+++ Positive-negative.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class PositiveNegative {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter your number:");
 		int n=s.nextInt();
